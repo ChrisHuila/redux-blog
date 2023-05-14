dbe2148bfd7cac0f6664405b56eddc825cb0c2cb
chrisHuila <christian.huila22@gmail.com>
2023-05-14 15:58:30 -0500

    skeleton mejorado y lazy load

--- src/components/search/ArticlesResult.jsx
+++ src/components/search/ArticlesResult.jsx
@@ -1,4 +1,5 @@
 import questionMarkImage from "../../assets/img/question-mark.jpg";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Skeleton from "@mui/material/Skeleton";
 
 import "./article.css";
@@ -14,28 +15,37 @@ const ArticlesResult = ({articles, loading}) => {
         <ul className="articles">
         {(loading ? Array.from(new Array(8)) : articles)
         .map((article, i) => (
-            article
-            ? <li className="article" key={article.id}>
-                <div className="articles-image-box">
-                    <a href={article.url} target="_blank">
-                        <img src={article.image? article.image :questionMarkImage} alt={article.title} />
-                    </a>
-                </div>
-                <p className="article-date">{getTime(article.publishedAt)}</p>
-                <p className="article-headline">{article.title}</p>
+            <li className="article" key={i}>
+                {article 
+                ?   <div className="articles-image-box">
+                        <a href={article.url} target="_blank">
+                            <LazyLoadImage 
+                            src={article.image? article.image :questionMarkImage}
+                            width={'100%'} height={'100%'}
+                            alt={article.title}
+                            placeholder={<Skeleton variant="rectangular" width="100%" height={300} />}
+                            />
+                            {/* <img  src={article.image? article.image :questionMarkImage} alt={article.title} /> */}
+                        </a>
+                    </div>
+                :   <div className="articles-image-box">
+                        <Skeleton variant="rectangular" width="100%" height={300} />
+                    </div>
+                }
+                {article 
+                ?   <>
+                        <p className="article-date">{getTime(article.publishedAt)}</p>
+                        <p className="article-headline">{article.title}</p>
+                    </>
+                :  
+                    <>
+                        <Skeleton width="60%" />
+                        <Skeleton />
+                    </>
+                }
             </li>
-            : (
-                <li className="article" key={i}>
-                <div className="articles-image-box">
-                    <Skeleton variant="rectangular" width="100%" height={300} />
-                </div>
-                <Skeleton width="60%" />
-                <Skeleton />
-            </li>
-              )
-        ))
-        }
-    </ul>
+            ))}
+        </ul>
     )
 }
 export default ArticlesResult;
